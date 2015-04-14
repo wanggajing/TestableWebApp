8c47b6338136eca36005135a86121672f614143b
Wu Ke <wke918@aucklanduni.ac.nz>
2015-04-14 15:49:01 +1200

    Added Unit test

---
+++ TestableWebApp.Tests/RSVPPresenterTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestableWebApp.Models.Repository;
+using TestableWebApp.Models;
+using System.Collections.Generic;
+using TestableWebApp.Presenters;
+using TestableWebApp.Presenters.Results;
+
+namespace TestableWebApp.Tests
+{
+    [TestClass]
+    public class RSVPPresenterTests
+    {
+        class MockRepository : IRepository
+        {
+            private List<GuestResponse> mockData = new List<GuestResponse> {
+                new GuestResponse {Name = "Person1", WillAttend = true},
+                new GuestResponse {Name = "Person2", WillAttend = false}
+            };
+            public IEnumerable<GuestResponse> GetAllResponse()
+            {
+                return mockData;
+            }
+            public List<GuestResponse> GetAllResponses()
+            {
+                return mockData;
+            }
+            public void AddResponse(GuestResponse response)
+            {
+                mockData.Add(response);
+            }
+        }
+        [TestMethod]
+        public void Adds_Object_To_Repository()
+        {
+            // Arrange
+            IRepository repo = new MockRepository();
+            IPresenter<GuestResponse> target = new RSVPPresenter { repository = repo };
+            GuestResponse dataObject =
+            new GuestResponse { Name = "TEST", WillAttend = true };
+            // Act
+            IResult result = target.GetResult(dataObject);
+            // Assert
+            Assert.AreEqual(repo.GetAllResponse().Count(), 3);
+            Assert.AreEqual(repo.GetAllResponse().Last().Name, "TEST");
+            Assert.AreEqual(repo.GetAllResponse().Last().WillAttend, true);
+        }
+    }
+}
--- TestableWebApp/Pages/Default.aspx.cs
+++ TestableWebApp/Pages/Default.aspx.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.ModelBinding;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using TestableWebApp.Models;
 using TestableWebApp.Models.Repository;
 using TestableWebApp.Presenters;
+using TestableWebApp.Presenters.Results;
 
 namespace TestableWebApp.Pages
 {
@@ -18,7 +20,11 @@ namespace TestableWebApp.Pages
             presenter = new RSVPPresenter {repository=new MemoryRepository()};
             if (IsPostBack)
             {
-
+                GuestResponse rsvp = ((DataResult<GuestResponse>)presenter.GetResult()).DataItem;
+                if (TryUpdateModel(rsvp, new FormValueProvider(ModelBindingExecutionContext)))
+                {
+                    Response.Redirect(((RedirectResult)presenter.GetResult(rsvp)).Url);
+                }
             }
         }
     }
